840c570427477f22812e0e0ffa976fe7122a9433
wajdan <wejdan95.almamari@gmail.com>
2026-05-12 08:35:00 +0400

    first program

--- Program.cs
+++ Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, WAJDAN !");
+
+            Console.WriteLine("ENTER YOUR NAME !");
+            int firstName = Console.Read();
+
         }
     }
 }
